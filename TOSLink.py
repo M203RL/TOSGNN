76ec45d0be6ed94513a47b1cb843467fd2400029
M203RL <125728183+M203RL@users.noreply.github.com>
2023-04-11 11:29:20 +0800

    Add files via upload

--- TOSLink.py
+++ TOSLink.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import random
 import time
 from pathlib import Path
+import os
 
 def formatTime(time):
     if time<10:
@@ -10,51 +11,69 @@ def formatTime(time):
     else:
         return str(time)
 
+cd = Path(__file__).parent.resolve()
+file = 'Link.txt'
+path = os.path.join(cd, file)
 target = '神魔之塔'
 tStart=time.time()
 t = time.localtime()
+years = int(time.strftime('%y', t))
 
 result = time.strftime("%Y/%m/%d %H:%M:%S", t)
-print("Start Time: "+result)
+print(f"Start Time: {result}")
 
-user_agent_list = ["Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; WOW64) Gecko/20100101 Firefox/61.0", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)", "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15"]
-file = 'Link.txt'
-open(file, 'w').close()
+user_agent_list = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15', 
+                   'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36', 
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', 
+                   'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36', 
+                   'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36', 
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36', 
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36', 
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.68', 
+                   'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36', 
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36']
+
+
+open(path, 'w').close()
+
+yearlength = years - 12
+
 idx = 0
-for k in range(2):
+for k in range(yearlength):
     for j in range(12):
-        year=23-k
-        month=12-j
+        year = years - k
+        month = 12 - j
+        url = f'https://gnn.gamer.com.tw/?yy=20{str(year)}&mm={str(month)}'
 
-        url='https://gnn.gamer.com.tw/?yy=20'+str(year)+'&mm='+str(month)
         headers = {'User-Agent': random.choice(user_agent_list)}
-        response = requests.get(url=url, headers=headers)
+        response = requests.get(url = url, headers = headers)
         soup = BeautifulSoup(response.text, 'lxml')
-        tart=soup.find_all("div", {"class": "GN-lbox2B"})
+        tart = soup.find_all("div", {"class": "GN-lbox2B"})
         for i in tart:
-            text=str(i)
+            text = str(i)
             if target in text:
-                dir=i.find('a')
+                dir = i.find('a')
                 if not 'https:' in dir:
-                    myLink = 'https:'+dir.get('href')
-                    response = requests.get(url=myLink, headers=headers)
-                    soup = BeautifulSoup(response.text, 'lxml')
-                    pArticle = soup.find("div", {"class": "BH-lbox GN-lbox3 gnn-detail-cont"})
-                    h1 = pArticle.find('h1').text.strip()
-                    fi = open(file, 'r',encoding='utf8')
-                    rec = fi.read()
-                    fi = open(file, 'w',encoding='utf8')
-                    fi.write(rec+h1+'\n'+myLink+'\n\n')
-                    fi.close()
-        tCurrent=time.time()
-        tLapsed=round(tCurrent-tStart)
+                    try:
+                        myLink = 'https:' + dir.get('href')
+                        response = requests.get(url = myLink, headers = headers)
+                        soup = BeautifulSoup(response.text, 'lxml')
+                        pArticle = soup.find("div", {"class": "BH-lbox GN-lbox3 gnn-detail-cont"})
+                        h1 = pArticle.find('h1').text.strip()
+                        fi = open(path, 'r', encoding = 'utf8')
+                        rec = fi.read()
+                        fi = open(path, 'w', encoding = 'utf8')
+                        fi.write(rec + h1 + '\n' + myLink + '\n\n')
+                        fi.close()
+                    except AttributeError:
+                        pass
+        tCurrent = time.time()
+        tLapsed = round(tCurrent - tStart)
         m, s = divmod(tLapsed, 60)
         h, m = divmod(m, 60)
-        h=formatTime(h)
-        m=formatTime(m)
-        s=formatTime(s)
-        print(f'Time Lapsed: {h}:{m}:{s}, Loops: {str(idx)}', end="\r")
-        delay_choices = [8, 5, 10, 6, 11, 9, 3, 1, 2, 7]  # 延遲的秒數
-        delay = random.choice(delay_choices)  # 隨機選取秒數
-        time.sleep(delay)
-        idx+=1
+        h = formatTime(h)
+        m = formatTime(m)
+        s = formatTime(s)
+        print(f'Time Lapsed: {h}:{m}:{s}, Loops: {str(idx)}(20{str(year)}/{str(month)})', end = "\r")
+        time.sleep(3)
+        idx += 1
