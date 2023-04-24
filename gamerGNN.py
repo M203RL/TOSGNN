e283ce50bfd71ea4115f506defb93ece09dedefa
M203RL <125728183+M203RL@users.noreply.github.com>
2023-04-24 17:36:51 +0800

    Add files via upload

--- gamerGNN.py
+++ gamerGNN.py
@@ -1,8 +1,18 @@
+import sys
+import subprocess
+import pkg_resources
+required = {'pyperclip', 'beautifulsoup4', 'requests', 'lxml', 'pyimgur', 'tqdm', 'selenium', 'webdriver-manager'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+if missing:
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+
+
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 from urllib.parse import quote
-
 import string
 import os
 import time
